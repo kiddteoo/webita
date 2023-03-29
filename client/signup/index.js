afc502ebb216f19e6d068928c072e2ecb45b0786
MrM250 <marcamor250@gmail.com>
2023-03-29 12:11:00 +0200

    pop

---
+++ client/signup/index.js
@@ -0,0 +1,12 @@
+var vue_app = new Vue({
+    el: "#app",
+    vuetify: new Vuetify(),
+    data: {
+        showPassword: false,
+        pass: '',
+        email: '',
+        birthdate: null,
+        menu: false,
+        
+    },
+})
