5c67ef3cff8e7ddcfe55519cddd8ceb9ef8fbbb0
meriendaa <a19teomerrod@inspedralbes.cat>
2023-04-16 18:54:42 +0200

    hola

---
+++ client/index5.js
@@ -0,0 +1,115 @@
+import * as THREE from 'three';
+import { OBJLoader } from './node_modules/three/examples/jsm/loaders/OBJLoader.js';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { MTLLoader } from './node_modules/three/examples/jsm/loaders/MTLLoader.js';
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+
+
+const container = document.getElementById('section5');
+container.width = "100%";
+
+const canvas = container.querySelector('canvas');
+document.body.style.touchAction = 'none';
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setPixelRatio(window.devicePixelRatio);
+canvas.width = "100%";
+canvas.height = container.offsetHeight;
+renderer.setSize(container.offsetWidth, container.offsetHeight);
+renderer.setClearColor( 0xffffff, 0);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.1;
+controls.rotateSpeed = 0.1;
+controls.minAzimuthAngle = -Math.PI/4;
+controls.maxAzimuthAngle = Math.PI/4; 
+controls.minPolarAngle = 0;
+controls.maxPolarAngle = Math.PI/2; 
+
+
+
+camera.position.z = 10.5;
+camera.position.x = -7.04;
+camera.position.y = 3;
+document.addEventListener('wheel', function(event) {
+	controls.enableZoom = false;
+	
+  })
+controls.update();
+
+
+/* function init(geometry){ */
+var textureLoader = new THREE.TextureLoader();
+
+var loader = new GLTFLoader();
+loader.load(
+	'./assets/noes.gltf', 
+	function (gltf) {
+
+	  
+		gltf.scene.rotation.x = 1.6;
+		gltf.scene.rotation.z = 0.7;
+		gltf.scene.rotation.y = 0;
+		gltf.scene.position.set(0, 3, 0);
+
+		
+		scene.add(gltf.scene);
+
+
+	},
+	function (xhr) {
+		console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+	},
+	function (error) {
+		console.error('Error loading GLTF model', error);
+	}
+);
+
+
+var ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+scene.add(ambientLight);
+
+
+var directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+directionalLight.position.set(0, 1, 0);
+scene.add(directionalLight);
+
+var pointLight = new THREE.PointLight(0xffffff, 2, 100);
+pointLight.position.set(0, 0, 10);
+scene.add(pointLight);
+
+function animate() {
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+	if (window.innerWidth < 770) {
+		scene.children[3].position.x = 0;
+		scene.children[3].position.y = 15;
+		scene.children[3].rotation.y = 0;
+	}
+
+	if (window.innerWidth > 770 && window.innerWidth < 1200) {
+		scene.children[3].position.x = -42;
+		scene.children[3].position.y = -20;
+		scene.children[3].rotation.y = 0;
+	}
+	
+	scene.children[3].rotation.z -= 0.01;
+
+
+}
+animate();
+
+
+function handleWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', handleWindowResize, false);
